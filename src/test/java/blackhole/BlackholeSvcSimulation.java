021bddb4ada599fde171811d48b008c15a270081
Cbhatt1 <>
2022-12-16 13:34:33 -0800

    Fixed main code

---
+++ src/test/java/blackhole/BlackholeSvcSimulation.java
@@ -0,0 +1,26 @@
+
+package blackhole;
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class BlackholeSvcSimulation extends Simulation {
+
+ HttpProtocolBuilder httpProtocol = http
+	.baseUrl("https://frontline-qal5-k8s.intuit.services.gateway.ppdmeshtestblackhole.mesh");
+
+ ScenarioBuilder scn = scenario("FrontlineMeshBlackholeScenario")
+		.exec(http("simpleGet")
+			.get("/get")
+			.check(status().is(200)));
+
+ { 
+   setUp(
+	scn.injectOpen(constantUsersPerSec(10).during (1600))).protocols(httpProtocol);
+ }
+
+}
